42e7b034942eeb3793a8a39c15aa2b01042968ff
hypeofpipe <hypeofpipe@gmail.com>
2017-04-10 15:23:09 +0200

    Created a project in libGDX-setup.jar

---
+++ core/src/com/hypeofpipe/racegame/MainClass.java
@@ -0,0 +1,20 @@
+package com.hypeofpipe.racegame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MainClass extends Game {
+
+	private MainClass(){
+
+	}
+
+	@Override
+	public void create() {
+
+	}
+}
