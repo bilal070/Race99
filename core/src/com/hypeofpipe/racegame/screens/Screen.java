cee5dcf95424ef13621182deabac3cc28712fe0a
hypeofpipe <hypeofpipe@gmail.com>
2017-05-08 13:07:44 +0200

    Added new assets

--- core/src/com/hypeofpipe/racegame/screens/Screen.java
+++ core/src/com/hypeofpipe/racegame/screens/Screen.java
@@ -51,7 +51,7 @@ public class Screen implements com.badlogic.gdx.Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-        
+
         world.step(1 / 60f, 6, 2);
         camera.update();
 
